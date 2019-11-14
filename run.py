8bf49904a127aff39a984f862462cd62f69092ef
Rita0702 <0702.t.kawakami@gmail.com>
2019-11-14 21:12:01 +0900

    run.pyをリファクタリング

--- run.py
+++ run.py
@@ -1,21 +1,4 @@
 from slackbot.bot import Bot
-from slackbot.bot import respond_to
-from slackbot.bot import listen_to
-import re
-from datetime import datetime
-import random
-
-
-@respond_to('今何時？')
-def now(message):
-    strftime = datetime.now().strftime(("%Y/%m/%d %H時%M分%S秒です！"))
-    message.reply(strftime)
-
-
-@respond_to('おみくじを引きたい！')
-def omikuji(message):
-    result = ['大吉', '吉', '中吉', '小吉', '末吉', '凶', '大凶']
-    message.reply(random.choice(result))
 
 
 def main():
