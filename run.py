3052935ff0a85ddeb0e0020a05a1d247474b0f27
Rita0702 <0702.t.kawakami@gmail.com>
2019-11-14 17:44:00 +0900

    botで現在時刻を返答してくれる機能追加
    モジュールをimport
    現在地獄を返す関数を定義

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 from slackbot.bot import Bot
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+import re
+from datetime import datetime
+
+@respond_to('今何時？')
+def now(message):
+    strftime = datetime.now().strftime(("%Y/%m/%d %H時%M分%S秒です！"))
+    message.reply(strftime)
 
 
 def main():
